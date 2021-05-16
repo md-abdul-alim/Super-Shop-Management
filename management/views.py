8bc91e57711283b40ee9c78af8d87ebb12587b82
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2021-05-16 18:43:47 +0600

    'pdf'

--- management/views.py
+++ management/views.py
@@ -136,7 +136,20 @@ def pdf_invoice_download(self, ref_code):
         "viewport-size": "1024x768",
     }
 
-    pdf_invoice = pdfkit.from_string(html, False, options=options)
+    ####################
+    import os, sys, subprocess, platform
+
+    if platform.system() == "Windows":
+        pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
+    else:
+        os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable) 
+        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], 
+            stdout=subprocess.PIPE).communicate()[0].strip()
+        pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+    pdf_invoice = pdfkit.from_string(html, False, options=options, configuration=pdfkit_config)
+    ####################
+
+    # pdf_invoice = pdfkit.from_string(html, False, options=options)
     filename = 'pdf-invoice-' + str(uuid.uuid4()) + '.pdf'
     response = HttpResponse(pdf_invoice, content_type="application/pdf")
     response['Content-Disposition'] = "attachment; filename=%s" % filename
